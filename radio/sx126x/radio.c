3371795d6888ee9dc206f80397591eb33093f403
Louis Thiery <louis@helium.com>
2019-11-25 11:06:58 -0800

    revert code to be closer to original

--- radio/sx126x/radio.c
+++ radio/sx126x/radio.c
@@ -906,6 +906,9 @@ uint32_t SX126xRadioTimeOnAir( RadioModems_t modem, uint8_t pktLen )
 
 void SX126xRadioSend( uint8_t *buffer, uint8_t size )
 {
+    // Initialize TCXO control (workaround since)
+    SX126xIoTcxoInit();
+
     SX126xSetDioIrqParams( IRQ_TX_DONE | IRQ_RX_TX_TIMEOUT,
                            IRQ_TX_DONE | IRQ_RX_TX_TIMEOUT,
                            IRQ_RADIO_NONE,
